84b7b7bd5f4b1cf88de04ca8b53fc3386c786983
Ally <bagkingally@gmail.com>
2017-06-20 19:54:09 +0100

    Move the binding of tic inside the Square Class

--- tictac.py
+++ tictac.py
@@ -4,13 +4,14 @@ class Square(tk.Canvas):
 	def __init__(self, master=None):
 		super().__init__(master)
 		self.pack()
+		self.bind("<Button-1>", self.tic
 	
 
 	def tic(self, event):
 		""""This will draw a nought or cross on the selected Square."""
 		self.create_line(0, 0, 200, 100)
 
+
 root = tk.Tk()
 square = Square(master=root)
-square.bind("<Button-1>", lambda event: Square.tic(square, event))
 root.mainloop()
