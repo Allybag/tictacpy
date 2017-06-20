927ef5ec45284ae614923f32bb45c37c14de52ea
Ally <bagkingally@gmail.com>
2017-06-20 20:41:19 +0100

    Working tic tac toe game! Still to be polished, but absolutely playable

--- tictac.py
+++ tictac.py
@@ -3,7 +3,6 @@ import tkinter as tk
 class Square(tk.Canvas):
 	def __init__(self, master=None, width=None, height=None):
 		super().__init__(master, width=width, height=height)
-		self.pack()
 		self.bind("<Button-1>", self.tic)
 		self.bind("<Button-2>", self.tac)
 	
@@ -18,5 +17,32 @@ class Square(tk.Canvas):
 		self.create_oval(30, 30, 170, 170)
 
 root = tk.Tk()
-square = Square(master=root, width=200, height=200)
+
+NW = Square(master=root, width=200, height=200)
+NW.grid(row=0, column=0)
+
+N = Square(master=root, width=200, height=200)
+N.grid(row=0, column=1)
+
+NE = Square(master=root, width=200, height=200)
+NE.grid(row=0, column=2)
+
+W = Square(master=root, width=200, height=200)
+W.grid(row=1, column=0)
+
+C = Square(master=root, width=200, height=200)
+C.grid(row=1, column=1)
+
+E = Square(master=root, width=200, height=200)
+E.grid(row=1, column=2)
+
+SW = Square(master=root, width=200, height=200)
+SW.grid(row=2, column=0)
+
+S = Square(master=root, width=200, height=200)
+S.grid(row=2, column=1)
+
+SE = Square(master=root, width=200, height=200)
+SE.grid(row=2, column=2)
+
 root.mainloop()
